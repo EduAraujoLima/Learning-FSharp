86165fb6ab1977f07e64a235731e05774ddbaeec
Eduardo Araújo <edu.wp@outlook.com>
2022-02-26 20:22:36 -0300

    Adicionar arquivos de projeto.

---
+++ Learning/Program.fs
@@ -0,0 +1,10 @@
+﻿
+open System
+
+let names = ["Peter"; "Julia"; "Xi"]
+
+let getGreeting name = $"Hello, {name}"
+
+names
+|> List.map getGreeting
+|> List.iter (fun greeting -> printfn $"{greeting} Enjoy your F#")
